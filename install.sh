c9c34b8d74823ade804c93b15b90388984ff1e3b
root <root@MSLab-WebServer.localdomain>
2020-03-19 13:49:44 +0800

    folder structure

---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -e
+echo "###update phase###"
+apt-get update
+apt-get upgrade -y
+set +e
+# In my distro(debian 10), It seems nginx and nginx-full are not compatible. I have to remove nginx than I can install nginx-full.
+apt-get remove -y nginx
+# The install script will detect npm exist or not on the system. If exist, it will not use itself's npm
+# But in Ubuntu 19.04, npm from apt are not compatible with it. So I have to remove first, and install back later.
+apt-get autoremove -y
+set -e
+echo "###install dependanse phase###"
+apt-get install -y nginx-full
+apt-get install -y libnginx-mod-http-auth-pam
+apt-get install -y lua5.2 lua5.2-doc liblua5.2-dev
+apt-get install -y luajit
+apt-get install -y libnginx-mod-http-lua
+apt-get install -y tmux gdbserver gdb git python python3 build-essential wget libncurses-dev nodejs 
+apt-get install -y python-pip python3-pip golang default-jdk
+apt-get install -y zsh fish tree ncdu aria2 p7zip-full python-dev python3-dev perl curl
+set +e # folling command only have one will success
+#cockpit for user management
+apt-get install -y -t bionic-backports cockpit cockpit-pcp #for ubuntu 18.04
+apt-get install -y cockpit cockpit-pcp                     #for ubuntu 19.04
+set -e
+pip3 install setuptools
+pip install  setuptools
+
+echo "###doenload files###"
+cd /etc
+git clone https://github.com/HuJK/Code-Server-Hub.git code-server-hub
+
+echo "###doenload latest code-server###"
+curl -s https://api.github.com/repos/cdr/code-server/releases/latest \
+| grep "browser_download_url.*linux-x86_64.tar.gz" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+echo "###set permission###"
+chmod -R 755 /etc/c9/sdk
+chmod -R 755 /etc/c9/.c9
+chmod -R 755 /etc/c9/util
+chmod -R 773 /etc/c9/sock
+HOME=/root
+
+set +e
+echo "###generate self signed cert###"
+echo "###You should buy or get a valid ssl certs           ###"
+echo "###Now I generate a self singed certs in /etc/c9/cert###"
+echo "###But you should replace it with valid a ssl certs  ###"
+echo '###Remember update your cert for cockpit too!        ###'
+echo '### cat ssl.pem ssl.key > /etc/cockpit/ws-certs.d/0-self-signed.cert###'
+apt-get install -y install openssl
+mkdir /etc/c9/cert
+chmod 600 /etc/c9/cert
+cd /etc/c9/cert
+openssl genrsa -out ssl.key 2048
+openssl req -new -x509 -key ssl.key -out ssl.pem -days 3650 -subj /CN=localhost
+cat ssl.pem ssl.key > /etc/cockpit/ws-certs.d/0-self-signed.cert
+
+echo "###restart nginx and cockpit###"
+systemctl enable nginx
+systemctl enable cockpit.socket
+service nginx stop
+service nginx start
+service cockpit stop
+service cockpit start
